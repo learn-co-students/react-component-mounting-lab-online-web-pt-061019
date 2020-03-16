184727e6033a0a7556984e1afa3367e85d546936
thistonyb <this.tonybaker@gmail.com>
2020-03-16 13:14:23 -0700

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -7,6 +7,14 @@ class Timer extends Component {
   };
 
   // add your code here
+  //Used once for instance at beginning to start timer after first render
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
 
   render() {
     const { time, color } = this.state;
